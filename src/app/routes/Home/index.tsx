ddfb483f0fd190094d6f477b25b354e537029a36
Liew Kok Siang <koksiang.liew@it.destijltech.com>
2026-05-01 19:12:58 +0800

    feat: add react icon and back button in homepage

--- src/app/routes/Home/index.tsx
+++ src/app/routes/Home/index.tsx
@@ -1,9 +1,23 @@
 import bannerImage from './banner.png'
+import { IoChevronBack } from 'react-icons/io5'
+import { closeEmbeddedApp } from '../../utils'
 import './banner.css'
 
 export function HomePage() {
+  const handleBack = () => {
+    if (window.parent && window.parent !== window) {
+      closeEmbeddedApp()
+      return
+    }
+
+    window.history.back()
+  }
+
   return (
     <section className="homeBanner" aria-label="Promotional banner">
+      <button type="button" className="homeBanner__backButton" onClick={handleBack} aria-label="Go back">
+        <IoChevronBack aria-hidden="true" />
+      </button>
       <img className="homeBanner__image" src={bannerImage} alt="World Cup 2026 promo" />
     </section>
   )
